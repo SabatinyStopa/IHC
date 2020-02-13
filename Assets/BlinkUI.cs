b3b73470e51e3d5ee39cad76ce11ca8dedb822d2
Sabatiny Gabriel Stopa <sabatinystopa@gmail.com>
2020-02-13 20:36:33 -0300

    First Drop
    Last drop provavelmente....

---
+++ Assets/BlinkUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class BlinkUI : MonoBehaviour{
+    public float m_SmoothTime = 10.0f;
+    private CanvasGroup m_Group;
+
+    private void Awake() {
+        m_Group = GetComponent<CanvasGroup>();    
+    }
+    
+    private void Update() {
+        m_Group.alpha = (Mathf.Sin(Time.time * m_SmoothTime) + 1.0f)  * 0.5f;
+    }
+}
---
+++ Assets/PingPongUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongUI : MonoBehaviour{
+    public float m_Range = 0.1f;
+    public float m_SmoothTime = 10.0f;
+    public Vector3 m_Axis = Vector3.up;
+    private Vector3 m_Origin;
+
+    private void Start() {
+        m_Origin = transform.position;
+    }
+
+    private void Update() {
+        transform.position = m_Origin + m_Axis * Mathf.Sin(Time.time * m_SmoothTime) * m_Range;
+    }
+}
