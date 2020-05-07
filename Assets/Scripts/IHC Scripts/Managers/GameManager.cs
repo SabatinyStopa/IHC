a7153227fff2ec925bfd12dc565eca45c71b6a72
Sabatiny Gabriel Stopa <sabatinystopa@gmail.com>
2020-05-07 20:49:05 -0300

    Tela de Pause e GameOver

---
+++ Assets/Scripts/IHC Scripts/Managers/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+    public HealthBar m_HealthBar;
+    public GameObject m_GameOver;
+
+    private void Update() {
+        if(m_HealthBar.m_Value <= 0)
+            m_GameOver.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/IHC Scripts/Managers/GameOver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour{
+   public Pause m_PauseScript;
+
+   private void OnEnable(){
+       m_PauseScript.Hide();
+       m_PauseScript.enabled = false;
+   }
+}
