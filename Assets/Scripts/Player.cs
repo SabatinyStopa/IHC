6b2c0bf1b6412c7eb4f1fc52c1d31a5359275ee3
Sabatiny Gabriel Stopa <sabatinystopa@gmail.com>
2020-05-14 20:46:10 -0300

    Minimapa

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+    public float m_Speed = 3.0f;
+    
+    private void Update(){
+        float x = Input.GetAxis("Horizontal") * m_Speed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * m_Speed * Time.deltaTime;
+        transform.Translate(new Vector3(x, 0.0f, z));
+    }
+}
